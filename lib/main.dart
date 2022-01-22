8c3878d47469cb595193a12d4260a9991badebf9
Rafael Morales <rmorales@utem.cl>
2022-01-22 17:07:44 -0300

    Small refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:mts/views/MyApp.dart';
+import 'package:mts/views/search.dart';
 
 void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Fetch data Example',
+      home: Search(),
+    );
+  }
+}
---
+++ lib/views/search.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:mts/classes/autocomplete_catalog.dart';
+import 'package:mts/views/cards/show.dart';
+
+class Search extends StatelessWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fetch Data Example'),
+      ),
+      body: TypeAheadField(
+        textFieldConfiguration: const TextFieldConfiguration(
+          autofocus: true,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+        ),
+        suggestionsCallback: (pattern) {
+          return fetchAutocompleteList(pattern);
+        },
+        itemBuilder: (context, String cardName) {
+          return ListTile(title: Text(cardName));
+        },
+        onSuggestionSelected: (String suggestion) async {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ShowCard(
+                cardName: suggestion,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
