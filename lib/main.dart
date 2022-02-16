431be4e01077714571c301099b7de14a945d8c96
Rafael Morales <rmorales@utem.cl>
2022-02-16 17:39:13 -0300

    Ligero restilizado de barra de busqueda, agregada BottomNav

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,51 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Fetch data Example',
-      // home: Testing(),
-      home: Search(),
+    return const MaterialApp(title: 'Fetch data Example', home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fetch Data Example'),
+      ),
+      body: const Search(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.collections_bookmark),
+            label: "Colección",
+            backgroundColor: Colors.lightBlue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: "Amigos",
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Perfil",
+            backgroundColor: Colors.purple,
+          )
+        ],
+      ),
     );
   }
 }
--- lib/views/search.dart
+++ lib/views/search.dart
@@ -8,31 +8,49 @@ class Search extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Fetch Data Example'),
-      ),
-      body: TypeAheadField(
-        textFieldConfiguration: const TextFieldConfiguration(
-          autofocus: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
+    return Container(
+      padding: const EdgeInsets.all(30.0),
+      child: SearchBar(),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  SearchBar({
+    Key? key,
+  }) : super(key: key);
+
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TypeAheadField(
+      textFieldConfiguration: TextFieldConfiguration(
+        controller: _controller,
+        autofocus: true,
+        decoration: InputDecoration(
+          border: const UnderlineInputBorder(),
+          suffixIcon: IconButton(
+            onPressed: _controller.clear,
+            icon: const Icon(Icons.clear),
+            splashColor: Colors.transparent,
           ),
+          hintText: '¿Qué carta buscas?',
         ),
-        suggestionsCallback: (pattern) => fetchAutocompleteList(pattern),
-        itemBuilder: (context, String cardName) =>
-            ListTile(title: Text(cardName)),
-        onSuggestionSelected: (String suggestion) async {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ShowCard(
-                cardName: suggestion,
-              ),
-            ),
-          );
-        },
       ),
+      suggestionsCallback: (pattern) => fetchAutocompleteList(pattern),
+      itemBuilder: (context, String cardName) =>
+          ListTile(title: Text(cardName)),
+      onSuggestionSelected: (String suggestion) async {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ShowCard(
+              cardName: suggestion,
+            ),
+          ),
+        );
+      },
     );
   }
 }
