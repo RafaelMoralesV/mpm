5ebeac8875ab6ad23ffd6bf08d9065476329f1dc
Rafael Morales <rmorales@utem.cl>
2022-02-17 19:45:03 -0300

    Making a card list view

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:mpmagic/classes/autocomplete_catalog.dart';
+import 'package:mpmagic/classes/mtg_card.dart';
+import 'package:mpmagic/views/cards/list.dart';
 import 'package:mpmagic/views/cards/show.dart';
 
 class Search extends StatelessWidget {
@@ -24,33 +26,50 @@ class SearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TypeAheadField(
-      textFieldConfiguration: TextFieldConfiguration(
-        controller: _controller,
-        autofocus: false,
-        decoration: InputDecoration(
-          border: const UnderlineInputBorder(),
-          suffixIcon: IconButton(
-            onPressed: _controller.clear,
-            icon: const Icon(Icons.clear),
-            splashColor: Colors.transparent,
+    return Row(
+      children: [
+        Expanded(
+          child: TypeAheadField(
+            textFieldConfiguration: TextFieldConfiguration(
+              controller: _controller,
+              autofocus: false,
+              decoration: InputDecoration(
+                border: const UnderlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: _controller.clear,
+                  icon: const Icon(Icons.clear),
+                  splashColor: Colors.transparent,
+                ),
+                hintText: '¿Qué carta buscas?',
+              ),
+            ),
+            suggestionsCallback: (pattern) => fetchAutocompleteList(pattern),
+            itemBuilder: (context, String cardName) =>
+                ListTile(title: Text(cardName)),
+            onSuggestionSelected: (String suggestion) async {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ShowCard(
+                    cardName: suggestion,
+                  ),
+                ),
+              );
+            },
           ),
-          hintText: '¿Qué carta buscas?',
         ),
-      ),
-      suggestionsCallback: (pattern) => fetchAutocompleteList(pattern),
-      itemBuilder: (context, String cardName) =>
-          ListTile(title: Text(cardName)),
-      onSuggestionSelected: (String suggestion) async {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ShowCard(
-              cardName: suggestion,
+        ElevatedButton(
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ListCards(
+                cardName: _controller.text,
+              ),
             ),
           ),
-        );
-      },
+          child: const Text("Go"),
+        ),
+      ],
     );
   }
 }
